24cc3dd55cccf44a9701fe57dd8f119729490d36
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2026-03-14 22:18:54 -0400

    fixed evaluator so it passes unit tests

--- src/cajal/evaluating.py
+++ src/cajal/evaluating.py
@@ -1,9 +1,8 @@
 from random import random
 from random import choice
-from collections.abc import Mapping
 from cajal.syntax import *
 
-type Env = Mapping[str, tuple[Val, Ty]]
+type Env = dict[str, Val]
 
 def evaluate(tm: Tm, env: Env) -> Val:
     match tm:
@@ -36,16 +35,13 @@ def evaluate(tm: Tm, env: Env) -> Val:
                 case _:
                     return VInj2(v, ty)
 
-        case TmDict(tm1, tm2):
-            v1 = evaluate(tm1, env)
-            v2 = evaluate(tm2, env)
-            match (v1, v2):
-                case (VError(), _):
-                    return VError()
-                case (_, VError()):
-                    return VError()
-                case _:
-                    return VDict(v1, v2)
+        case TmDict(tm1s, tm2s):
+            v1s = [evaluate(tm1, env) for tm1 in tm1s]
+            v2s = [evaluate(tm2, env) for tm2 in tm2s]
+            if (VError() in v1s) or (VError() in v2s):
+                return VError()
+            else:
+                return VDict(v1s, v2s)
                 
         case TmCase(tm, x1, tm1, x2, tm2):
             v = evaluate(tm, env)
@@ -56,7 +52,9 @@ def evaluate(tm: Tm, env: Env) -> Val:
                     return evaluate(tm1, env | {x1: v1})
                 case VInj2(v2, ty):
                     return evaluate(tm2, env | {x2: v2})
-                
+                case _:
+                    raise ValueError(f"Unexpected value: {v}")
+
         case TmProj1(tm):
             v = evaluate(tm, env)
             match v:
@@ -64,6 +62,8 @@ def evaluate(tm: Tm, env: Env) -> Val:
                     return VError()
                 case VPair(tm1, tm2):
                     return evaluate(tm1, env)
+                case _:
+                    raise ValueError(f"Unexpected value: {v}")
 
         case TmProj2(tm):
             v = evaluate(tm, env)
@@ -72,7 +72,9 @@ def evaluate(tm: Tm, env: Env) -> Val:
                     return VError()
                 case VPair(tm1, tm2):
                     return evaluate(tm2, env)
-                
+                case _:
+                    raise ValueError(f"Unexpected value: {v}")
+
         case TmChoice(tm1, tm2):
             if random() < .5:
                 return evaluate(tm1, env)
@@ -80,12 +82,12 @@ def evaluate(tm: Tm, env: Env) -> Val:
                 return evaluate(tm2, env)
             
         case TmLet(x, tm1, tm2):
-            v1 = evaluate(tm1)
+            v1 = evaluate(tm1, env)
             return evaluate(tm2, env | {x: v1})
         
         case TmLookup(tm1, tm2, rel):
-            v1 = evaluate(tm1)
-            v2 = evaluate(tm2)
+            v1 = evaluate(tm1, env)
+            v2 = evaluate(tm2, env)
             match (v1, v2):
                 case (VError(), _):
                     return VError()
@@ -93,7 +95,9 @@ def evaluate(tm: Tm, env: Env) -> Val:
                     return VError()
                 case (VDict(ks, vs), q):
                     V = [v for (k,v) in zip(ks, vs) if rel(k,q)]
-                    return choice(V)
-
-        case _:
-            raise ValueError(f"Unexpected term: {tm}")
+                    if V:
+                        return choice(V)
+                    else:
+                        return VError()
+                case _:
+                    raise ValueError(f"Unexpected values: {v1}, {v2}")
