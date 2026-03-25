cd501efc67f67f020373239068ae64fc51bd7bb3
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2026-03-25 17:06:56 -0500

    draft of compiler forr Unit + Sum + Prod fragment

--- src/cajal/compiling.py
+++ src/cajal/compiling.py
@@ -75,20 +75,133 @@ def compile(tm: Tm) -> nn.Module:
             return NnLet()
 
         case TmCase(tm_sum, xs, tms):
+            
+            match tm_sum.ty_checked:
+                case TySum(tys):
 
-            class NnCase(nn.Module):
+                    ptr = 0
+                    slices = []
+                    for i in range(len(tys)):
+                        offset = dim(tys[i])
+                        slices.append((ptr, ptr + offset))
+                        ptr += offset
+
+                    class NnCase(nn.Module):
+                        def __init__(self):
+                            super().__init__()
+                            self.tm_sum_compiled = compile(tm_sum)
+                            self.tms_compiled = nn.ModuleList([compile(tm) for tm in tms])
+                        def forward(self, env: Env):
+                            v_sum = self.tm_sum_compiled(env)
+                            vs = []
+                            for i in range(len(self.tms_compiled)):
+                                v_i = v_sum[slices[i][0]: slices[i][1]]
+                                vs.append(self.tms_compiled[i](env | {xs[i]: v_i}))
+                            return torch.stack(vs).sum(dim=0)
+                    
+                    return NnCase()
+                        
+                case _:
+                    raise TypeError(f"{tm_sum=} is not a sum type: {tm_sum.ty_checked=}.")
+                
+                
+        case TmSeq(_, tm2):
+
+            class NnSeq(nn.Module):
                 def __init__(self):
                     super().__init__()
-                    self.tm_sum_compiled = compile(tm_sum)
-                    self.tms_compiled = nn.ModuleList([compile(tm) for tm in tms])
+                    self.tm2_compiled = compile(tm2)
                 def forward(self, env: Env):
-                    v_sum = self.tm_sum_compiled(env)
-                    return ...
-                
-            return NnCase()
+                    return self.tm2_compiled(env)
+
+            return NnSeq()
+        
+        case TmProj(n, tm_prod):
+            match tm_prod.ty_checked:
+                case TyProd(tys):
+
+                    ptr = 0
+                    slices = []
+                    for i in range(len(tys)):
+                        offset = dim(tys[i])
+                        slices.append((ptr, ptr + offset))
+                        ptr += offset
+
+                    class NnProj(nn.Module):
+                        def __init__(self):
+                            super().__init__()
+                            self.tm_prod_compiled = compile(tm_prod)
+                        def forward(self, env: Env):
+                            v_prod = self.tm_prod_compiled(env)
+                            return v_prod[slices[n][0]: slices[n][1]]
                     
+                    return NnProj()
+
+                case _:
+                    raise TypeError(f"{tm_prod=} is not a prod type: {tm_prod.ty_checked=}.")
+
         case _:
             raise NotImplementedError()
+
+
+
+def compile_val(v: Val) -> nn.Module:
+    match v:
+        case VUnit():
+
+            class NnUnit(nn.Module):
+                def __init__(self):
+                    super().__init__()
+                    self.value = nn.Parameter(torch.tensor([1.0]))
+                def forward(self, _: Env) -> torch.Tensor:
+                    return self.value
+
+            return NnUnit()
+
+        case VError():
+
+            class NnError(nn.Module):
+                def __init__(self):
+                    super().__init__()
+                    self.value = nn.Parameter(torch.tensor([0.0]))
+                def forward(self, _: Env) -> torch.Tensor:
+                    return self.value
+
+            return NnError()
+
+        case VInj(m, v_sum, TySum(tys)):
+
+            class NnInj(nn.Module):
+                def __init__(self):
+                    super().__init__()
+                    self.v_compiled = compile_val(v_sum)
+                    self.zero_left: torch.Tensor
+                    self.zero_right: torch.Tensor
+                    self.register_buffer('zero_left', zero(TySum(tys[:m])))
+                    self.register_buffer('zero_right', zero(TySum(tys[m+1:])))
+                def forward(self, env: Env) -> torch.Tensor:
+                    return torch.cat([self.zero_left, self.v_compiled(env), self.zero_right])
+            
+            return NnInj()
+
+
+        case VProd(tms):
+
+            class NnProd(nn.Module):
+                def __init__(self):
+                    super().__init__()
+                    self.tms_compiled = nn.ModuleList([compile(tm) for tm in tms])
+                def forward(self, env: Env) -> torch.Tensor:
+                    return torch.cat([tm_compiled(env) for tm_compiled in self.tms_compiled])
+
+            return NnProd()
+
+        case VDict(v1, v2):
+            raise NotImplementedError()
+
+        case _:
+            raise NotImplementedError()
+
 
 # --- Helpers ---
 
