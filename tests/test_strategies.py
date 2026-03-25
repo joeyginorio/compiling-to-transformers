65fbb311d75231abdf173e6ee142466581a0f24d
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2026-03-25 17:08:07 -0500

    added a check to make sure value generator emits well typed values

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,10 +1,20 @@
+import hypothesis.strategies as st
 from hypothesis import given, settings, HealthCheck
-from strategies import gen_prog
-from cajal.typing import check
+from strategies import gen_prog, gen_val, gen_ty
+from cajal.typing import check, check_val
+
+# ============= `gen_tm`: Property-based Testing
 
 @settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow])
 @given(gen_prog())
 def test_gen_tm(prog):
     ctx, tm, ty = prog
     ty_check = check(tm, ctx.flat())
+    assert ty == ty_check
+
+@settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow])
+@given(gen_ty(), st.data())
+def test_gen_val(ty, data):
+    val = data.draw(gen_val(ty, set()))
+    ty_check = check_val(val)
     assert ty == ty_check
