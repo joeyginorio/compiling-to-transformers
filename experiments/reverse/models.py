705599654b63121977a3062c0b04b2fbfb02c21a
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2026-04-02 15:43:10 -0500

    fixed bug in ModelD() due to bad reshaping

--- experiments/reverse/models.py
+++ experiments/reverse/models.py
@@ -2,22 +2,70 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from experiments.reverse.dataset import decode
+import cajal.compiling as cj
+from cajal.typing import check
+from cajal.syntax import TmProd, TmProj, TmVar, TySum, TyUnit, TyProd
+
+# --- Program ---
+
+elem_ty = TySum([TyUnit()] * 4)
+reverse = TmProd([TmProj(29 - i, TmVar('x')) for i in range(30)])
+check(reverse, {'x': TyProd([elem_ty] * 30)})
+reverse_module = cj.compile(reverse)
+
 
 # --- Models ---
 
 class ModelD(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size: int = 27, d_model: int = 16, n_heads: int = 4, seq_len: int = 30):
         super().__init__()
+        self.register_buffer('mask', nn.Transformer.generate_square_subsequent_mask(seq_len))
+        self.register_buffer('positions', torch.arange(seq_len))
 
-    def forward(self, x):
-        return x
+        self.tok_emb = nn.Embedding(vocab_size, d_model)
+        self.pos_emb = nn.Embedding(seq_len, d_model)
+        self.attn = nn.MultiheadAttention(d_model, n_heads, batch_first=True)
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, 4 * d_model),
+            nn.ReLU(),
+            nn.Linear(4 * d_model, d_model),
+        )
+        self.out = nn.Linear(d_model, vocab_size)
+
+        d_head = d_model // n_heads
+        self.rev = reverse_module
+        self.rev_proj_in = nn.Linear(d_model, d_head, bias=False)
+        self.merge = nn.Linear(d_model + d_head, d_model, bias=False)
+
+    def forward(self, x: torch.Tensor, representations: bool = False) -> torch.Tensor | tuple[torch.Tensor, dict[str, torch.Tensor]]:
+        x1 = self.tok_emb(x) + self.pos_emb(self.positions)
+        h1, _ = self.attn(x1, x1, x1, attn_mask=self.mask, is_causal=True, need_weights=False)
+        rev_in = self.rev_proj_in(x1)
+        rev_out = torch.vmap(self.rev)({'x': rev_in.flatten(start_dim=1)}).reshape(rev_in.shape)
+        r1 = self.merge(torch.cat([h1, rev_out], dim=-1)) + x1
+        r2 = self.mlp(r1) + r1
+        logits = self.out(r2)
+
+        if representations:
+            return logits, {"x1": x1, "h1": h1, "r1": r1, "r2": r2, "rev_in": rev_in, "rev_out": rev_out}
+        else:
+            return logits
+
+class ModelU(nn.Module):
+    def __init__(self, model_d: nn.Module):
+        super().__init__()
+        self.model = cj.to_multilinear(model_d)
+    
+    def forward(self, env: cj.Env) -> torch.Tensor:
+        return self.model(env)
 
 class ModelT(nn.Module):
-    def __init__(self):
+    def __init__(self, model_d: nn.Module):
         super().__init__()
-
-    def forward(self, x):
-        return x
+        self.model = cj.to_multilinear(model_d, rand=True)
+    
+    def forward(self, env: cj.Env) -> torch.Tensor:
+        return self.model(env)
 
 class ModelI(nn.Module):
     def __init__(self, vocab_size: int = 27, d_model: int = 16, n_heads: int = 4, seq_len: int = 30):
